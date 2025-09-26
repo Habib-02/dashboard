3bccb0e32743399fb449055757313d2998feb139
habib <moh.habibur.7@gmail.com>
2025-09-26 12:23:01 +0300

    style aside links

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,5 +1,12 @@
 import styles from "./SideMenu.module.css";
 
+import { LayoutDashboard } from "lucide-react";
+import { ShoppingBasket } from "lucide-react";
+import { Heart } from "lucide-react";
+import { List } from "lucide-react";
+import { Mails } from "lucide-react";
+import { Warehouse } from "lucide-react";
+
 function SideMenu() {
   return (
     <aside className={styles.sideMenu}>
@@ -13,32 +20,38 @@ function SideMenu() {
         <ul className={styles.navItems}>
           <li className={styles.navItem}>
             <a className={styles.navLink} href="">
-              Dashboard
+              <LayoutDashboard className={styles.icon} />
+              <span>Dashboard</span>
             </a>
           </li>
           <li className={styles.navItem}>
             <a className={styles.navLink} href="">
-              Products
+              <ShoppingBasket />
+              <span> Products</span>
             </a>
           </li>
           <li className={styles.navItem}>
             <a className={styles.navLink} href="">
-              Favorites
+              <Heart />
+              <span>Favorites</span>
             </a>
           </li>
           <li className={styles.navItem}>
             <a className={styles.navLink} href="">
-              Inbox
+              <Mails />
+              <span>Inbox</span>
             </a>
           </li>
           <li className={styles.navItem}>
             <a className={styles.navLink} href="">
-              Order Lists
+              <List />
+              <span>Order Lists</span>
             </a>
           </li>
           <li className={styles.navItem}>
             <a className={styles.navLink} href="">
-              Products Stock
+              <Warehouse />
+              <span>Products Stock</span>
             </a>
           </li>
         </ul>
